c9ea349b40f21d982226822fdb53062ec15f7af6
heshamamohamed <heshamamohamed323@gmail.com>
2022-05-29 20:01:34 +0200

    added registrations controller

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::RegistrationsController < Devise::RegistrationsController
+  def create
+    user = User.create(sign_up_params)
+
+    if user.save
+      render json: { token: JsonWebToken.encode(sub: user.id) }
+    else
+      render json: { errors: user.errors.full_messages }
+    end
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :image)
+  end
+end
