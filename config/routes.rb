19e1ad7073a551e6c1b56dba005d1c903af9a592
heshamamohamed <heshamamohamed323@gmail.com>
2022-05-29 20:39:17 +0200

    added login routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       devise_scope :api_v1_user do
         post 'users/register', to: 'registrations#create'
+        post 'users/login', to: 'sessions#create'
       end
     end
   end
