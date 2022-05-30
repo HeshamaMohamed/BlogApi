c4487dd1827bf242bf498fb0243be70dd09b703e
heshamamohamed <heshamamohamed323@gmail.com>
2022-05-30 06:02:29 +0200

    added user has many posts relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+  has_many :posts, dependent: :destroy
 
   validates :name, presence: true, length: { within: 6..32 }, format: { with: /\A[a-zA-Z\s]*\Z/, message: 'can contain alphabets only' }
 end
