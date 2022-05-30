54311f725c7cc3c5f56e5495d7f41b070bc5bfe6
heshamamohamed <heshamamohamed323@gmail.com>
2022-05-30 21:43:29 +0200

    added dependency for user->comments and posts->comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: { within: 6..32 }, format: { with: /\A[a-zA-Z\s]*\Z/, message: 'can contain alphabets only' }
 end
