e662e4de988c0c0bd6311a18a4d2124186ea6f17
heshamamohamed <heshamamohamed323@gmail.com>
2022-05-30 06:07:30 +0200

    changed posts table body column to text instead of string

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_29_193922) do
+ActiveRecord::Schema.define(version: 2022_05_30_040534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
-    t.string "body", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
