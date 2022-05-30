84c4463ed5c6875c28e460d85dd0e485a004bb94
heshamamohamed <heshamamohamed323@gmail.com>
2022-05-30 08:21:40 +0200

    added migration join table posts_tags for many-many relation between Posts and Tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_041336) do
+ActiveRecord::Schema.define(version: 2022_05_30_045900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2022_05_30_041336) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
+    t.index ["post_id", "tag_id"], name: "index_posts_tags_on_post_id_and_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
